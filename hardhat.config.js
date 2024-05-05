39852b3982b470d5b769732fda94bf187c40f0a6
KOSASIH <kosasihg88@gmail.com>
2024-05-05 17:22:18 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 11095000
+      }
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
+};
