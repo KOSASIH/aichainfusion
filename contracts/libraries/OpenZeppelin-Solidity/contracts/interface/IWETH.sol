726141be067fb6458097e8f820b933d9e4887e21
KOSASIH <kosasihg88@gmail.com>
2024-05-05 16:47:49 +0700

    Create IWETH.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/interface/IWETH.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IWETH {
+    function deposit() external payable;
+    function withdraw(uint256 wad) external;
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
