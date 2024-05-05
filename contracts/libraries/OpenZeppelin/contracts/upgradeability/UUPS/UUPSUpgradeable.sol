ed08ae03b9abd4a5181a0bcfaa7a1d4f5083551e
KOSASIH <kosasihg88@gmail.com>
2024-05-05 14:44:38 +0700

    Create UUPSUpgradeable.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/upgradeability/UUPS/UUPSUpgradeable.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract UUPSUpgradeable is Initializable {
+    address public implementation;
+
+    constructor() Initializable {
+        implementation = address(this);
+    }
+
+    function upgradeTo(address _newImplementation) public {
+        require(msg.sender == implementation, "Only the current implementation can upgrade");
+        implementation = _newImplementation;
+    }
+
+    function call(bytes memory _data) public returns (bytes memory) {
+        return implementation.call(_data);
+    }
+}
