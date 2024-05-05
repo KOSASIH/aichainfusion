418dfd650e024cd99272882c68f0a40e18dfbc87
KOSASIH <kosasihg88@gmail.com>
2024-05-05 14:01:28 +0700

    Create ERC20.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/token/ERC20/ERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyERC20 is ERC20 {
+    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
