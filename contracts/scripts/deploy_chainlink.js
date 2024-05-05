0ff333749baa2898736100a5ff7d306939630527
KOSASIH <kosasihg88@gmail.com>
2024-05-05 17:01:17 +0700

    Create deploy_chainlink.js

---
+++ contracts/scripts/deploy_chainlink.js
@@ -0,0 +1,17 @@
+async function deployChainlink() {
+  // Initialize your contract factory here, for example:
+  const contractFactory = await ethers.getContractFactory('YourChainlinkContract');
+
+  // Deploy your contract here, for example:
+  const yourChainlinkContract = await contractFactory.deploy();
+  await yourChainlinkContract.deployed();
+
+  console.log(`YourChainlinkContract deployed to: ${yourChainlinkContract.address}`);
+}
+
+deployChainlink()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
