92fd7cbbc569ff4c54173ba7584e4d3bf5e799d4
KOSASIH <kosasihg88@gmail.com>
2024-05-05 16:43:05 +0700

    Create IERC721.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/interface/IERC721.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IERC721 {
+    function balanceOf(address owner) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function safeTransferFrom(address from, address to, uint256 tokenId) external payable;
+    function getApproved(uint256 tokenId) external view returns (address);
+    function setApprovalForAll(address operator, bool approved) external;
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
