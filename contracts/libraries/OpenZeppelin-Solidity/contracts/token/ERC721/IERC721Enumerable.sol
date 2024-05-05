91e810f9ab38b25a1ad59677a27e7678cc257495
KOSASIH <kosasihg88@gmail.com>
2024-05-05 15:42:29 +0700

    Create IERC721Enumerable.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/token/ERC721/IERC721Enumerable.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+interface IERC721Enumerable is IERC721 {
+    function totalSupply() external view returns (uint256);
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+    function tokenByIndex(uint256 index) external view returns (uint256);
+}
