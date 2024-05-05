682e357785033362d2b2e076ab02e014407bfac4
KOSASIH <kosasihg88@gmail.com>
2024-05-05 16:45:40 +0700

    Create IERC1155Receiver.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/interface/IERC1155Receiver.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IERC1155Receiver {
+    function onERC1155Received(address operator, address from, uint256 id, uint256 value, bytes calldata data) external returns (bytes4);
+    function onERC1155BatchReceived(address operator, address from, uint256[] memory ids, uint256[] memory values, bytes calldata data) external returns (bytes4);
+}
