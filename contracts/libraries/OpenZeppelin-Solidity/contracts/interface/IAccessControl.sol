209d41829386bbed4abbb6004a71983f369bb008
KOSASIH <kosasihg88@gmail.com>
2024-05-05 16:46:46 +0700

    Create IAccessControl.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/interface/IAccessControl.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IAccessControl {
+    function hasRole(bytes32 role, address account) external view returns (bool);
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+    function grantRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+    function renounceRole(bytes32 role, address account) external;
+}
