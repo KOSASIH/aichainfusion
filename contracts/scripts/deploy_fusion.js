d2fcfc606ae7560d0e3bc3e2fa89b548bdd2efbb
KOSASIH <kosasihg88@gmail.com>
2024-05-05 17:01:53 +0700

    Create deploy_fusion.js

---
+++ contracts/scripts/deploy_fusion.js
@@ -0,0 +1,17 @@
+async function deployFusion() {
+  // Initialize your contract factory here, for example:
+  const contractFactory = await ethers.getContractFactory('YourFusionContract');
+
+  // Deploy your contract here, for example:
+  const yourFusionContract = await contractFactory.deploy();
+  await yourFusionContract.deployed();
+
+  console.log(`YourFusionContract deployed to: ${yourFusionContract.address}`);
+}
+
+deployFusion()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
