e6a4f79dd2f983f545c013711db07e5921c3b60a
KOSASIH <kosasihg88@gmail.com>
2024-05-05 14:09:04 +0700

    Create ERC721.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/token/ERC721/ERC721.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyERC721 is ERC721 {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+}
