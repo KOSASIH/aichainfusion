c848deea1b15cbec9a057b92164482830a2603b6
KOSASIH <kosasihg88@gmail.com>
2024-05-05 13:59:31 +0700

    Create IERC20Metadata.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/token/ERC20/IERC20Metadata.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+interface IERC20Metadata is IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+}
