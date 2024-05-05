91660e0f9463b8b19d58407ba5fd4f282c2ed893
KOSASIH <kosasihg88@gmail.com>
2024-05-05 16:48:20 +0700

    Create ITransferReceiver.sol

---
+++ contracts/libraries/OpenZeppelin-Solidity/contracts/interface/ITransferReceiver.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface ITransferReceiver {
+    function onERC1155Received(address operator, address from, uint256 id, uint256 value, bytes calldata data) external returns (bytes4);
+    function onERC1155BatchReceived(address operator, address from, uint256[] memory ids, uint256[] memory values, bytes calldata data) external returns (bytes4);
+}
