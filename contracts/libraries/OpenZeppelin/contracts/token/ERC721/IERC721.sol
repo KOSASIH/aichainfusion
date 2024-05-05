1be467c159038bc60ef436758add0540dbf2c25e
KOSASIH <kosasihg88@gmail.com>
2024-05-05 14:04:04 +0700

    Create IERC721.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/token/ERC721/IERC721.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address owner) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function approve(address to, uint256 tokenId) external returns (bool);
+    function getApproved(uint256 tokenId) external view returns (address);
+    function setApprovalForAll(address operator, bool approved) external;
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+    function transferFrom(address from, address to, uint256 tokenId) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+}
