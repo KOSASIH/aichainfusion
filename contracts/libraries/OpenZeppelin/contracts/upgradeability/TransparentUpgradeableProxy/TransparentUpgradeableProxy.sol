58688cc65bdfd41f5ea63e12acf458f847d31ca5
KOSASIH <kosasihg88@gmail.com>
2024-05-05 14:41:25 +0700

    Create TransparentUpgradeableProxy.sol

---
+++ contracts/libraries/OpenZeppelin/contracts/upgradeability/TransparentUpgradeableProxy/TransparentUpgradeableProxy.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract TransparentUpgradeableProxy is Initializable {
+    address public admin;
+    address public implementation;
+
+    constructor(address _implementation) Initializable {
+        admin = msg.sender;
+        implementation = _implementation;
+    }
+
+    function upgradeTo(address _newImplementation) public {
+        require(msg.sender == admin, "Only admin can upgrade the contract");
+        implementation = _newImplementation;
+    }
+
+    function call(bytes memory _data) public returns (bytes memory) {
+        return implementation.call(_data);
+    }
+}
